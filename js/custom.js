0d332423eb37804f19f1069bedd612e8555e46d6
alsamzu <zulfalsam@gmail.com>
2018-02-16 23:15:27 +0300

    custom js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,10 @@
 alert ("Hello Javascript");
+var myVar="Alsam is ..."
+document.write(myVar)
+var myAge=27;
+if(myAge==24){
+    document.write("\n You are Alsam")
+}
+else{
+    document.write("\n You are Zulfa")
+}
